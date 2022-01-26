4ac90e721be01bcda1bc7a5bc0480684db990d42
francisco <fj23pk@hotmail.com>
2022-01-25 18:03:41 -0600

    binding implemnted and textView added to xml

--- app/src/main/java/com/TenmaFrank/happybirthday/MainActivity.kt
+++ app/src/main/java/com/TenmaFrank/happybirthday/MainActivity.kt
@@ -1,11 +1,16 @@
-package com.TenmaFrank.happybirthday
+package com.tenmafrank.happybirthday
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.tenmafrank.happybirthday.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView<ActivityMainBinding>(this,R.layout.activity_main)
     }
 }
